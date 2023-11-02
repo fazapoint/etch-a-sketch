a3d55567fc67a22a0985517c4aa4d054a5869187
Fazarama <53871962+fazapoint@users.noreply.github.com>
2023-11-02 21:10:30 +0700

    adding 16x16 grid in the script.js
    using grid-template to create the length of the grid

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const gridContainer = document.querySelector('#grid-container');
 
-for (let i = 0; i < 16; i++){
-    const grid = document.createElement('div');
-    grid.classList.add('grid-item');
-    grid.textContent = `test ${i+1}`;
-    gridContainer.appendChild(grid);
-};
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridItem.textContent = `${i + 1}, ${j + 1}`; // Optional: display row and column numbers
+        gridContainer.appendChild(gridItem);
+    }
+}
