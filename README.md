f19811031c5edeee1a42e7a32ea39df97a8f3dd4
Faza Ramadhani <53871962+fazapoint@users.noreply.github.com>
2023-11-03 18:23:57 +0700

    complete basic functionality
    adding slider for size input of the grid, sketch with single click or when mouse held down, and color picker button

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # etch-a-sketch
 etch a sketch in javascript for the odin project submission! You can see the live preview [here](https://fazapoint.github.io/etch-a-sketch/)
+
+What i did in this project:
+- Create blank canvas using grid
+- Create slider to input the size of the grid dynamically
+- Using mousedown, mouseup and mouseover event to be able to sketch when mouse is held down or clicked
+- Create array to store colors and pass each of them one by one to the button value
--- script.js
+++ script.js
@@ -1,13 +1,95 @@
 const gridContainer = document.querySelector('#grid-container');
+let isMouseDown = false;
+const colors = ['black', 'red', 'green', 'blue', 'pink', 'purple'];
+// color selector initialize with 1 because 0 already used as a default
+let colorIndex = 1;
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridContainer.appendChild(gridItem);
+function createGrid(numGrid){
+    // clear grid container
+    clearGrid();
 
-        gridItem.addEventListener('mouseover', () => {
-            gridItem.style.backgroundColor = 'red';
-        })
+    // create x number of columns and rows
+    gridContainer.style.gridTemplateColumns = `repeat(${numGrid}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${numGrid}, 1fr)`
+    for (let i = 0; i < numGrid; i++) {
+        for (let j = 0; j < numGrid; j++) {
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gridContainer.appendChild(gridItem);
+            
+            // when mouseover, if isMousDown is true then can sketch
+            gridItem.addEventListener('mouseover', () => {
+                if (isMouseDown){
+                    gridItem.style.backgroundColor = btnColorPick.value;
+                }
+            });
+
+            // when a single gridItem is clicked
+            gridItem.addEventListener('click', () => {
+                gridItem.style.backgroundColor = btnColorPick.value;
+            });
+        }
     }
 }
+
+function clearGrid(){
+    gridContainer.innerHTML = '';
+    // alternative way to remove child element
+    // while (gridContainer.firstChild) {
+    //     gridContainer.removeChild(gridContainer.firstChild);
+    // }
+}
+
+// slider
+const slider = document.querySelector('#sizeSlider');
+const sliderValueDisplay = document.querySelector('#sliderValue');
+
+// display current value of the slider
+slider.addEventListener('input', () => {
+    const sliderValue = slider.value;
+    sliderValueDisplay.textContent = `${sliderValue}x${sliderValue}`;
+});
+
+// execute createGrid() when value is changed
+slider.addEventListener('change', () => {
+    const sliderValue = slider.value;
+    createGrid(sliderValue);
+});
+
+// mousedown to detect isMousDown is true
+gridContainer.addEventListener('mousedown', (event) => {
+    isMouseDown = true;
+
+    // prevent cursor turn into grab icon when mouse held down
+    event.preventDefault();
+});
+
+// mouseup to detect isMouseDown is false
+gridContainer.addEventListener('mouseup', () => {
+    isMouseDown = false;
+});
+
+// clear button event
+const btnClear = document.querySelector('#btnClear');
+btnClear.addEventListener('click', () => {
+    clearGrid();
+    createGrid(slider.value);
+})
+
+const btnColorPick = document.querySelector('#btnColorPick');
+btnColorPick.addEventListener('click', () => {
+    btnColorPick.value = colors[colorIndex];
+    btnColorPick.textContent = `color: ${colors[colorIndex]}`;
+
+    // pass each color in colors array
+    if (colorIndex < colors.length-1){
+        colorIndex++;
+    } else{
+        colorIndex = 0;
+    }
+});
+
+// initialize the grid 16x16 size
+createGrid(slider.value);
+
+
