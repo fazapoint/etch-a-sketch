daedbbaf4f80716b187b1da5b52b227f892588e7
Faza Ramadhani <53871962+fazapoint@users.noreply.github.com>
2023-11-03 12:36:34 +0700

    create hover effect using addEventListener
    the grid will change background color if mouse over it

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
-        gridItem.textContent = `${i + 1}, ${j + 1}`; // Optional: display row and column numbers
         gridContainer.appendChild(gridItem);
+
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.style.backgroundColor = 'red';
+        })
     }
 }
